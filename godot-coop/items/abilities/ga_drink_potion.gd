384fbd8f2267d3ebe7e081d857cec2aa1b5540b5
Andrés Barros Rivas <andresbr92@gmail.com>
2026-02-20 10:36:03 +0100

    update repomix

--- godot-coop/items/abilities/ga_drink_potion.gd
+++ godot-coop/items/abilities/ga_drink_potion.gd
@@ -1,6 +1,8 @@
 class_name GA_Drink_Potion
 extends GameplayAbility
 
+@export var potion_data: PotionData
+
 
 func activate(actor: Node, handle: AbilitySpecHandle, args: Dictionary = {}) -> void:
 	GlobalLogger.log("drinking potion")
